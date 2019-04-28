07aaad97262aab99bc5f7d588a29f204f224ea81
Michal Opalka <michalopalka998@gmail.com>
2019-04-28 13:03:49 +0200

    Added searching function

--- src/js/editor/notes-sidebar/main.js
+++ src/js/editor/notes-sidebar/main.js
@@ -1,50 +1,42 @@
+import {
+   normalizeText
+} from './../../utilities';
+import * as searchBox from './search-box';
+
 const notesListEl = document.querySelector('#notesListEl');
 
 const notesList = [{
-   tittle: 'Notatka 1',
-   lastModified: '27/04/2019 | 14:30'
-}, {
-   tittle: 'Notatka 1',
-   lastModified: '27/04/2019 | 14:30'
-}, {
-   tittle: 'Notatka 1',
-   lastModified: '27/04/2019 | 14:30'
-}, {
-   tittle: 'Notatka 1',
-   lastModified: '27/04/2019 | 14:30'
-}];
+      tittle: 'Notatka 1',
+      lastModified: '27/04/2019 | 14:30'
+   },
+   {
+      tittle: 'Notatka 2',
+      lastModified: '27/04/2019 | 14:30'
+   },
+   {
+      tittle: 'Notatka 3',
+      lastModified: '27/04/2019 | 14:30'
+   },
+   {
+      tittle: 'Notatka 4',
+      lastModified: '27/04/2019 | 14:30'
+   }
+];
 
-const generateNoteHTML = (note) => {
-   return `<li class="list-group-item d-flex justify-content-between lh-condensed">
-   <div>
-      <h6 class="my-0">${note.tittle}</h6>
-      <small class="text-muted">${note.lastModified}</small>
-   </div>
-</li>`
-}
+const generateNotesList = () => {
+   const generateNoteHTML = note => {
+      return `<li class="list-group-item d-flex justify-content-between lh-condensed">
+      <div>
+         <h6 class="my-0">${note.tittle}</h6>
+         <small class="text-muted">${note.lastModified}</small>
+      </div>
+   </li>`;
+   };
 
-const html = notesList.map(note => generateNoteHTML(note)).join('');
+   const html = notesList.filter(note => normalizeText(note.tittle).includes(normalizeText(searchBox.getSearchPhrase()))).map(note => generateNoteHTML(note)).join('');
 
-notesListEl.innerHTML = html;
+   notesListEl.innerHTML = html;
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*                   <li class="list-group-item d-flex justify-content-between lh-condensed">
-                     <div>
-                        <h6 class="my-0">Product name</h6>
-                        <small class="text-muted">Brief description</small>
-                     </div>
-                  </li> */
+generateNotesList();
+searchBox.init(() => generateNotesList());
