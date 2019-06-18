a11e678a5a9b6d657eba13f455a9238b7e60cd1c
Michal Opalka <michalopalka998@gmail.com>
2019-06-18 23:39:22 +0200

    Added data-base to download

--- src/js/axios-api.js
+++ src/js/axios-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'https://my-json-server.typicode.com/Offblack/markdown-notepad'
+   baseURL: 'https://localhost:7001'
 });
 
 export default instance;
